f9e241a839fe274c778a535f5febd61f2c33c312
uuronn <80239312+uuronn@users.noreply.github.com>
2021-08-06 22:53:47 +0900

    変数test

--- c_practice/main.cpp
+++ c_practice/main.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
+
 int main(void)
 {
-	printf("helloWorld");
+	int left;
+	int right;
+	left = 10;
+	right = 3;
+
+	printf("%d\n",left + right);
+	printf("%d\n",left - right);
+	printf("%d\n",left * right);
+	printf("%d\n",left / right);
+	printf("%d\n",left % right);
 	return 0;
 }
